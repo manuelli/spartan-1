d21af39d471c934b2cc396a18654132ab8eaf4d2
Yunzhu Li <liyunzhu@mit.edu>
2018-01-25 23:17:44 -0500

    demo version (fusion + external force) on real robot

--- yunzhu/scripts/external_force_monitor.py
+++ yunzhu/scripts/external_force_monitor.py
@@ -5,16 +5,22 @@ import std_msgs.msg
 import sensor_msgs.msg
 
 
-diff_threshold = 10
+diff_threshold = 6
 
 
 class ExternalForceMonitor(object):
 
     def __init__(self, diff_threshold):
         self.pub = rospy.Publisher("/stop", std_msgs.msg.Bool, queue_size=10)
+        rospy.Subscriber("/stop", std_msgs.msg.Bool, self.encounter_stop_signal)
         rospy.Subscriber("/joint_states", sensor_msgs.msg.JointState, self.callback)
         self.first_time = True
         self.diff_threshold = diff_threshold
+        self.sent_signal = False
+
+    def encounter_stop_signal(self, msg):
+        if msg.data:
+            self.sent_signal = True
 
     def callback(self, msg):
         self.external_force = np.array(list(msg.effort))
@@ -25,19 +31,21 @@ class ExternalForceMonitor(object):
         else:
             diff = np.sum(np.square(self.external_force_ref - self.external_force))
 
-            print "ref: ", self.external_force_ref
-            print "cur: ", self.external_force, diff
+            # print "ref: ", self.external_force_ref
+            # print "cur: ", self.external_force, diff
 
-            if diff > self.diff_threshold:
+            if not self.sent_signal and diff > self.diff_threshold:
+                self.sent_signal = True
                 self.pub.publish(True)
                 print "publising"
-                time.sleep(5)
+                time.sleep(0.01)
                 self.pub.publish(False)
                 print "stop publising"
-                time.sleep(10)
 
 
 if __name__ == '__main__':
+
+    time.sleep(1.0)
 
     rospy.init_node('external_force_monitor')
     external_force_monitor = ExternalForceMonitor(diff_threshold)
